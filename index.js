86ebe22942a42377ec0bae9ecb1df5f67b50ff80
zhangning <zhangning@zhangningdeMacBook-Pro.local>
2019-11-12 17:33:42 +0800

    test

---
+++ index.js
@@ -0,0 +1,17 @@
+const program = require('commander');
+var process1 = require('child_process');
+
+program.version('1.0.0', '-v, --version')
+  .command('init <name>')
+  .action((name) => {
+    console.log('clone template ...');
+    process1.exec('git clone https://github.com/ainama/ainama-react.git' + name, function(error, stdout, stderr) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        return;
+      }
+      console.log(stdout);
+      console.log('clone success');
+    });
+  });
+program.parse(process.argv);
